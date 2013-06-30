c3b7d7213535da034a5e06fe98ed1de2721001a5
IcmpEcho <pinnapong@gmail.com>
2013-06-30 21:43:35 +0700

    Add fib.rb

---
+++ fib.rb
@@ -0,0 +1,13 @@
+require 'benchmark'
+
+def fib(x)
+	x < 2 ? x : fib(x-1) + fib(x-2)
+end
+
+def run_benchmark(n)
+	for i in 1..n
+		puts "fib(#{i}): " + Benchmark.measure{ fib(i) }.to_s
+	end
+end
+
+run_benchmark(10)
